fff304aae9063a405deefce4962d6b155f5d427e
MarcinMysliwiec <marcin.mysliw@gmail.com>
2021-02-11 13:23:09 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/Sleeping_Barber_Problem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sleeping_Barber_Problem.dir/main.cpp.o"
+  "Sleeping_Barber_Problem"
+  "Sleeping_Barber_Problem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sleeping_Barber_Problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sleeping_Barber_Problem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sleeping_Barber_Problem.
+# This may be replaced when dependencies are built.
